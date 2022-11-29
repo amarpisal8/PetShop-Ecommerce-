bd7949026d392c3acf2980c43326d1432167cace
amarpisal8 <109473825+amarpisal8@users.noreply.github.com>
2022-11-29 19:02:24 +0000

    Just For Study

---
+++ OddAndEven.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;  // Import the Scanner class
+
+import java.util.*;
+class OddAndEven {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter a Number");
+    int n=sc.nextInt();
+
+  }
+}
